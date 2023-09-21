8a4b695dc639c922a67348e89e978e24aa32b20c
aleksandar-pejkovic <aleksandar.pejkovic89@gmail.com>
2023-09-21 13:00:51 +0200

    add infinite scroll

---
+++ src/components/Character.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="border rounded-3xl m-5 p-5 flex flex-col items-center">
+        <img class="rounded-3xl w-80" :src="character.image" :alt="character.name">
+        <h2 class="text-lg font-semibold">{{ character.name }}</h2>
+        <p>{{ character.species }}</p>
+        <p>Status: {{ character.status }}</p>
+        <p>Created: {{ character.created }}</p>
+        <p>Location: {{ character.location.name }}</p>
+        <p>Origin: {{ character.origin.name }}</p>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    character: Object
+})
+</script>
--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,38 +1,72 @@
 <template>
-    <div class="flex flex-wrap">
-        <div v-for="character in characters" :key="character.id" class="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4 p-4">
-            <div class="border rounded p-4 flex flex-col items-center">
-                <img :src="character.image" :alt="character.name">
-                <h2 class="text-lg font-semibold">{{ character.name }}</h2>
-                <p>{{ character.species }}</p>
-                <p>Status: {{ character.status }}</p>
-                <p>Created: {{ character.created }}</p>
-                <p>Location: {{ character.location.name }}</p>
-                <p>Origin: {{ character.origin.name }}</p>
-            </div>
-        </div>
+    <div ref="scrollComponent" class="flex flex-wrap justify-around overflow-y-auto max-h-screen">
+        <h1 class="text-center p-10 text-5xl font-bold text-red-700 w-full">Characters from Rick and Morty</h1>
+        <Character v-for="character in characters" :key="character.id" :character="character" />
     </div>
 </template>
   
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, onUnmounted } from 'vue';
 import axios from 'axios';
+import Character from './Character.vue';
 
+const scrollComponent = ref(null);
 const characters = ref([]);
+const info = ref(null);
+const initialUri = ref('https://rickandmortyapi.com/api/character');
+const nextPageNumber = ref(1)
 
-const fetchData = () => {
+const fetchData = (uri) => {
+    nextPageNumber.value++
     axios
-        .get('https://rickandmortyapi.com/api/character')
+        .get(uri)
         .then((response) => {
-            characters.value = response.data.results;
+            characters.value.push(...response.data.results);
+            info.value = response.data.info;
         })
         .catch((error) => {
             console.error('Error fetching data:', error);
         });
 };
 
+const fetchMoreData = () => {
+    const hasNextPage = info.value && info.value.next
+    if (hasNextPage) {
+        const url = new URL(info.value.next);
+        const queryParams = new URLSearchParams(url.search);
+        const urlPageNumber = queryParams.get('page');
+        const nextPageIsNotLoaded = nextPageNumber.value == urlPageNumber;
+        if (nextPageIsNotLoaded) {
+            fetchData(info.value.next);
+        }
+    }
+};
+
 onMounted(() => {
-    fetchData();
+    fetchData(initialUri.value);
+    scrollComponent.value.addEventListener('scroll', handleScroll)
 });
+
+onUnmounted(() => {
+    scrollComponent.value.removeEventListener('scroll', handleScroll)
+})
+
+let previousScrollTop = 0;
+
+const handleScroll = (e) => {
+    let containerElement = scrollComponent.value;
+    const scrollTop = containerElement.scrollTop;
+    const scrollHeight = containerElement.scrollHeight;
+    const clientHeight = containerElement.clientHeight;
+    const scrollDifference = scrollTop - previousScrollTop;
+    const bottomHeight = scrollTop + clientHeight;
+    const customScrollLineOffset = scrollHeight - 300;
+    const isScrollingDown = scrollDifference > 0;
+
+    if (bottomHeight >= customScrollLineOffset && isScrollingDown) {
+        fetchMoreData();
+    }
+    previousScrollTop = scrollTop;
+};
 </script>
   
